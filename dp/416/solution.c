a9e668e0ed17b077645feb64e8a7501faa08bc9d
lymi <269442394@qq.com>
2019-09-23 15:17:17 +0800

    optimization

--- dp/416/solution.c
+++ dp/416/solution.c
@@ -3,37 +3,56 @@
 
 /**
  * 1. DP状态定义:
- *    DP[i][j] 代表取索引i之前的任意多个数字相加能否为j
+ *    DP[i][j] 代表取前i+1个数中任意多个数字相加之和能否为j
  *    0 -- 不能，1 -- 能
- * 2. DP转移方程:
+  2. DP转移方程:
  *    DP[i][j] = DP[i-1][j] || DP[i-1][j-nums[i]]
+ *    空间优化后:
+ *    DP[j] = DP[j] || DP[j-nums[i]]
  */
 int canPartition(int* nums, int N) {
   if (N <= 1) return 0;
-
   int sum = 0;
-
   for (int i = 0; i < N; i++) sum += nums[i];
-
   if (sum % 2 != 0) return 0;
-  
   int target = sum / 2;
   int DP[N][target+1];
 
-  for (int i = 0; i < N; i++) {
-    for (int j = 1; j <= target; j++) {
-      DP[i][j] = 0;
-    }
-    DP[i][0] = 1;
+  DP[0][0] = 1;
+ 
+  for (int j = 1; j <= target; j++) {
+    DP[0][j] = nums[0] == j ? 1 : 0;
   }
 
   for (int i = 1; i < N; i++) {
-    for (int j = target; j >= nums[i]; j--) {
-      DP[i][j] = DP[i-1][j] || DP[i-1][j-nums[i]];
+    for (int j = 0; j <= target; j++) {
+      if (j >= nums[i]) {
+        DP[i][j] = DP[i-1][j] || DP[i-1][j-nums[i]];
+      } else {
+        DP[i][j] = DP[i-1][j];
+      }
     }
   }
 
   return DP[N-1][target];
+
+  /*********** 空间优化 **************
+  int DP[target+1];
+
+  for (int i = 1; i <= target; i++) {
+    DP[i] = 0;
+  }
+  
+  DP[0] = 1; 
+
+  for (int i = 1; i < N; i++) {
+    for (int j = target; j >= nums[i]; j--) {
+      DP[j] = DP[j] || DP[j-nums[i]];
+    }
+  }
+
+  return DP[target];
+  ************************************/
 }
 
 int main() {
--- dp/494/solution.c
+++ dp/494/solution.c
@@ -3,44 +3,68 @@
 
 /**
  * 1. DP状态定义:
- *    DP[i][j] 表示在前i+1个数字中选择任意多个数字, 
- *    相加等于j最多j有多少种选法。
+ *    DP[i][j] 表示在前i+1个数字中选择任意多个(包括0个)数字, 
+ *    相加结果等于 j 最多有多少种选法。
  * 2. DP转移方程:
  *    DP[i][j] = DP[i-1][j] + DP[i-1][j-nums[i]];
+ *    空间优化后:
+ *    DP[j] = DP[j] + DP[j-nums[i]]
  */
-
 int findTargetSumWays(int* nums, int N, int S) {
   int sum = 0;
-
   for (int i = 0; i < N; i++) sum += nums[i];
   if (sum < S || (sum - S) % 2 != 0) return 0;
-
   int target = (sum - S) / 2;
-  int DP[N][target + 1];
+  int DP[N][target+1];
 
-  for (int i = 0; i < N; i++) {
-    for (int j = 1; j <= target; j++) {
-      if (j == nums[i]) {
-        DP[i][j] = 1;
-      } else {
-        DP[i][j] = 0;
-      }
-    }
-    DP[i][0] = 1;
+  if (nums[0] == 0) {
+    DP[0][0] = 2;
+  } else {
+    DP[0][0] = 1;
+  }
+
+  for (int j = 1; j <= target; j++) {
+    DP[0][j] = nums[0] == j ? 1 : 0;
   }
 
   for (int i = 1; i < N; i++) {
-    for (int j = target; j >= nums[i]; j--) {
-      DP[i][j] = DP[i-1][j] + DP[i-1][j-nums[i]];
+    for (int j = 0; j <= target; j++) {
+      if (j >= nums[i]) {
+        DP[i][j] = DP[i-1][j] + DP[i-1][j-nums[i]];
+      } else {
+        DP[i][j] = DP[i-1][j];
+      }
     }
   }
 
   return DP[N-1][target];
+
+  /************* 空间优化 ***************
+  int DP[target+1];
+
+  for (int i = 1; i <= target; i++) {
+    DP[i] = 0;
+  }
+
+  DP[0] = 1;
+
+  for (int i = 0; i < N; i++) {
+    for (int j = target; j >= nums[i]; j--) {
+      DP[j] = DP[j] + DP[j-nums[i]];
+    } 
+  }
+
+  return DP[target];
+  ***************************************/
 }
 
 int main() {
-  int nums[5] = {1, 1, 1, 1, 1};
-  assert(findTargetSumWays(nums, 5, 3) == 5);
+  int nums1[5] = {1,1,1,1,1};
+  assert(findTargetSumWays(nums1, 5, 3) == 5);
+  printf("===============\n");
+  int nums2[9] = {0,0,0,0,0,0,0,0,1};
+  assert(findTargetSumWays(nums2, 9, 1) == 256);
+
   printf("ALL TESTS PASSED!\n");
   return 0;
 }
