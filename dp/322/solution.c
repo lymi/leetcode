fd70b61090daed5fbb5c1fa7a76776144ce4e5de
lymi <269442394@qq.com>
2019-09-26 10:59:42 +0800

    accomplish

---
+++ dp/322/solution.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <assert.h>
+
+/**
+ * 1. DP状态定义:
+ *    DP[i] 代表构成面额i最少需要的硬币数
+ * 2. DP转移方程:
+ *    DP[i] = 1 + min(DP[i-coins[j]])
+ *                   (0 <= j < N)
+ */
+
+int coinChange(int *coins, int N, int amount) {
+  int DP[amount+1];
+
+  for (int i = 0; i <= amount; i++) {
+    DP[i] = -1;
+    for (int j = 0; j < N; j++) {
+      if (coins[j] == i) {
+        DP[i] = 1;
+        break;
+      }
+    }
+  }
+
+  for (int i = 0; i <= amount; i++) {
+    int temp = -1;
+    for (int j = 0; j < N; j++) {
+      if (i < coins[j]) continue;
+      if (temp == -1 || temp > DP[i-coins[j]]) {
+        temp = DP[i-coins[j]];
+      }
+    }
+
+    if (temp > 0) {
+      DP[i] = 1 + temp;
+    }
+  }
+
+  return DP[amount];
+}
+
+int main() {
+  int coins1[3] = {1,2,5};
+  assert(coinChange(coins1, 3, 11) == 3);
+  int coins2[1] = {2};
+  assert(coinChange(coins2, 1, 3) == -1);
+
+  printf("ALL TESTS PASSED!\n");
+  return 0;
+}
+
