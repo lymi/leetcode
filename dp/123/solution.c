32ed9c104bbd30a73bcaf6af8764927fc8e836fe
lymi <269442394@qq.com>
2019-09-26 14:58:18 +0800

    stock 3

--- dp/123/solution.c
+++ dp/123/solution.c
@@ -4,7 +4,6 @@
 /**
  * 思路同P188，只需将 K 替换成2。
  */
-
 int max(int x, int y, int z) {
   int ret = x > y ? x : y;
   return ret > z ? ret : z;
@@ -21,12 +20,10 @@ int maxProfit(int *prices, int N) {
         ret += prices[i] - prices[i-1];
       }
     }
-
     return ret;
   }
 
   int DP[3][N+1];
-
   for (int k = 0; k <= 2; k++) {
     for (int i = 0; i < N; i++) {
       DP[k][i] = 0;
@@ -51,10 +48,8 @@ int maxProfit(int *prices, int N) {
 int main() {
   int arr1[8] = {3,3,5,0,0,3,1,4};
   assert(maxProfit(arr1, 8) == 6);
-
   int arr2[5] = {1,2,3,4,5};
   assert(maxProfit(arr2, 5) == 4);
-
   int arr3[5] = {7,6,4,3,1};
   assert(maxProfit(arr3, 5) == 0);
   
