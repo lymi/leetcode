51cc8d49102413e9be506c2d384d82d7f36a7bc3
lymi <269442394@qq.com>
2019-09-28 20:16:27 +0800

    ones and zeros

--- dp/474/solution.c
+++ dp/474/solution.c
@@ -7,15 +7,15 @@
  *    DP[i][m][n] 表示前i+1个字符串中, 满足'0'的总数不超过m, '1'的
  *    总数不超过n, 最多有多少个字符串.
  * 2. DP转移方程:
- *    DP[i][m][n] = max(DP[i-1][m][n], DP[i-1][m-count(strs[i],'0')][n-count(strs[i],'1')] + 1)
+ *    DP[i][m][n] = max(DP[i-1][m][n], DP[i-1][m-count('0')][n-count('1')] + 1)
+ *    空间优化:
+ *    DP[m][n] = max(DP[m][n], 1 + DP[m-count('0')][n-count('1')])
  */
 
 int count(char *str, char c) {
   int cnt = 0;
   for (int i = 0; i < (int)strlen(str); i++) {
-    if (str[i] == c) {
-      cnt++;
-    }
+    if (str[i] == c) cnt++;
   }
   return cnt;
 }
@@ -25,33 +25,25 @@ int max(int x, int y) {
 }
 
 int findMaxForm(char **strs, int S, int M, int N) {
-  int DP[S][M+1][N+1];
+  int DP[M+1][N+1];
 
   for (int m = 0; m <= M; m++) {
     for (int n = 0; n <= N; n++) {
-      if (count(strs[0], '0') <= m && count(strs[0], '1') <= n) {
-        DP[0][m][n] = 1;
-      } else {
-        DP[0][m][n] = 0;
+      DP[m][n] = 0;
+    }
+  }
+
+  for (int i = 0; i < S; i++) {
+    int cnt0 = count(strs[i], '0');
+    int cnt1 = count(strs[i], '1');
+    for (int m = M; m >= cnt0; m--) {
+      for (int n = N; n >= cnt1; n--) {
+        DP[m][n] = max(DP[m][n], 1 + DP[m-cnt0][n-cnt1]);
       }
     }
   }
 
-  for (int i = 1; i < S; i++) {
-    for (int m = 0; m <= M; m++) {
-      for (int n = 0; n <= N; n++) {
-        int cnt0 = count(strs[i], '0');
-        int cnt1 = count(strs[i], '1');
-        if (m >= cnt0 && n >= cnt1) {
-          DP[i][m][n] = max(DP[i-1][m][n], DP[i-1][m-cnt0][n-cnt1] + 1);
-        } else {
-          DP[i][m][n] = DP[i-1][m][n];
-        }
-      }
-    }
-  }
-
-  return DP[S-1][M][N];
+  return DP[M][N];
 }
 
 int main() {
