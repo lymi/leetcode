8eb053bc0c90e475eb164c0c8573a84b81af0c1b
lymi <269442394@qq.com>
2019-09-28 19:47:38 +0800

    target sum

--- dp/494/solution.c
+++ dp/494/solution.c
@@ -16,30 +16,7 @@ int findTargetSumWays(int* nums, int N, int S) {
   for (int i = 0; i < N; i++) sum += nums[i];
   if (sum < S || (sum - S) % 2 != 0) return 0;
   int target = (sum - S) / 2;
-  int DP[N][target+1];
 
-  if (nums[0] == 0) {
-    DP[0][0] = 2;
-  } else {
-    DP[0][0] = 1;
-  }
-
-  for (int j = 1; j <= target; j++) {
-    DP[0][j] = nums[0] == j ? 1 : 0;
-  }
-
-  for (int i = 1; i < N; i++) {
-    for (int j = 0; j <= target; j++) {
-      if (j >= nums[i]) {
-        DP[i][j] = DP[i-1][j] + DP[i-1][j-nums[i]];
-      } else {
-        DP[i][j] = DP[i-1][j];
-      }
-    }
-  }
-  return DP[N-1][target];
-
-  /************* 空间优化 ***************
   int DP[target+1];
 
   for (int i = 1; i <= target; i++) {
@@ -49,19 +26,17 @@ int findTargetSumWays(int* nums, int N, int S) {
   DP[0] = 1;
 
   for (int i = 0; i < N; i++) {
-    for (int j = target; j >= nums[i]; j--) {
+    for (int j = nums[i]; j <= target; j++) {
       DP[j] = DP[j] + DP[j-nums[i]];
     } 
   }
 
   return DP[target];
-  ***************************************/
 }
 
 int main() {
   int nums1[5] = {1,1,1,1,1};
   assert(findTargetSumWays(nums1, 5, 3) == 5);
-  printf("===============\n");
   int nums2[9] = {0,0,0,0,0,0,0,0,1};
   assert(findTargetSumWays(nums2, 9, 1) == 256);
 
