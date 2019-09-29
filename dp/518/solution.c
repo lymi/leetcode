6a3d5d01fdee458f86681f63011cce44fbda2481
lymi <269442394@qq.com>
2019-09-29 22:07:55 +0800

    coin change 2

--- dp/518/solution.c
+++ dp/518/solution.c
@@ -3,10 +3,9 @@
 
 /**
  * 1. DP状态定义
- *    DP[i] 表示构成面额 i 一共多少种组合方式。
+ *    DP[i] 表示选择硬币构成面额i一共多少种选法, 每种面值的硬币可以选无限多
  * 2. DP转移方程
  *    DP[i] = SUM(DP[i-coins[j]])
- *    为避免重复计算，将 j 作为外层循环
  */
 int change(int amount, int *coins, int N) {
   int DP[amount+1];
@@ -16,12 +15,12 @@ int change(int amount, int *coins, int N) {
     DP[i] = 0;
   }
 
-  for (int j = 0; j < N; j++) {
-    for (int i = coins[j]; i <= amount; i++) {
-      DP[i] += DP[i-coins[j]];
+  for (int i = 0; i < N; i++) {
+    for (int j = coins[i]; j <= amount; j++) {
+      DP[j] += DP[j-coins[i]];
     }
   }
-  
+
   return DP[amount];
 }
 
@@ -29,7 +28,6 @@ int main() {
   int coins1[3] = {1,2,5};
   int coins2[1] = {2};
   int coins3[1] = {10};
-
   assert(change(5, coins1, 3) == 4);
   assert(change(3, coins2, 1) == 0);
   assert(change(10, coins3, 1) == 1);
