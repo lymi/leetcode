e99f5ec1f46052304b57f5877d5b0a07520ab5c6
lymi <269442394@qq.com>
2019-09-29 15:42:58 +0800

    coin change

--- dp/322/solution.c
+++ dp/322/solution.c
@@ -24,8 +24,9 @@ int coinChange(int *coins, int N, int amount) {
   
   for (int i = 1; i <= amount; i++) {
     for (int j = 0; j < N; j++) {
-      if (i < coins[j]) continue;
-      DP[i] = min(DP[i], 1 + DP[i-coins[j]]);
+      if (i >= coins[j]) {
+        DP[i] = min(DP[i], 1 + DP[i-coins[j]]);
+      }
     }
   }
 
