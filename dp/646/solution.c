be5872ef1893e223b4a9fc445479e70ac0f1204b
lymi <269442394@qq.com>
2019-09-30 15:32:33 +0800

    max length of pair chain

--- dp/646/solution.c
+++ dp/646/solution.c
@@ -4,9 +4,9 @@
 /**
  * 1. DP状态定义:
  *    DP[i]: 对于索引 0 -> i(i: 0 -> N-1) 的pairs序列, 在选中 i 的情况下,
- *    能够构成的最长链条长度。                         ^^^^^^^^^^^^^^^^^
+ *    能够构成的最长链条长度。
  * 2. DP转移方程:
- *    DP[i] = max{DP[j]} + 1   (pairs[i][0] > pairs[j][LEN-1])   (j: 0 -> i-1)
+ *    DP[i] = max{DP[j]} + 1   (pairs[i]在链头或链尾)   (j: 0 -> i-1)
  * 3. 最终结果 max{DP[i]} (i: 0 -> N-1)
  */
 int findLongestChain(int **pairs, int N, int *LENS){
@@ -17,7 +17,8 @@ int findLongestChain(int **pairs, int N, int *LENS){
   for (int i = 0; i < N; i++) {
     max = 0;
     for (int j = 0; j < i; j++) {
-      if (max < DP[j] && pairs[i][0] > pairs[j][LENS[j]-1]) {
+      if (max < DP[j] && (pairs[i][0] > pairs[j][LENS[j]-1] ||
+            pairs[i][LENS[i]-1] < pairs[j][0])) {
         max = DP[j];
       }
     }
@@ -35,9 +36,12 @@ int main() {
   int pair1[2] = {1,2};
   int pair2[2] = {2,3};
   int pair3[2] = {3,4};
-  int *pairs[3] = {pair1, pair2, pair3};
+  int *pairs1[3] = {pair1, pair2, pair3};
+  int *pairs2[3] = {pair3, pair2, pair1};
   int lens[3] = {2,2,2};
-  assert(findLongestChain(pairs, 3, lens) == 2);
+
+  assert(findLongestChain(pairs1, 3, lens) == 2);
+  assert(findLongestChain(pairs2, 3, lens) == 2);
 
   printf("ALL TESTS PASSED!\n");
   return 0;
