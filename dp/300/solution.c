e430782f6ba35440d7571fe2c095ed8b29a41487
lymi <269442394@qq.com>
2019-09-30 14:22:11 +0800

    longest increasing subsequence

--- dp/300/solution.c
+++ dp/300/solution.c
@@ -3,7 +3,7 @@
 
 /**
  * 1. DP状态定义:
- *    DP[i]: 对于给定序列0 -> i (i: 1 -> N-1)，在选中元素 i 的情况下，
+ *    DP[i]: 对于给定序列0 -> i (i: 0 -> N-1)，在选中元素 i 的情况下，
  *    最长递增子序列的长度
  * 2. DP转移方程:
  *    DP[i] = max{DP[j]} + 1   (nums[i] > nums[j])   (j: 0 -> i-1)
@@ -16,7 +16,7 @@ int lengthOfLIS(int *nums, int N) {
 
   DP[0] = 0;
   
-  for (int i = 1; i < N; i++) {
+  for (int i = 0; i < N; i++) {
     max = 0;
     for (int j = 0; j < i; j++) {
       if (max < DP[j] && nums[i] > nums[j]) {
@@ -36,8 +36,10 @@ int lengthOfLIS(int *nums, int N) {
 int main() {
   int nums1[8] = {10,9,2,5,3,7,101,18};
   int nums2[0] = {};
+  int nums3[1] = {0};
   assert(lengthOfLIS(nums1, 8) == 4);
   assert(lengthOfLIS(nums2, 0) == 0);
+  assert(lengthOfLIS(nums3, 1) == 1);
 
   printf("ALL TESTS PASSED!\n");
   return 0;
