ec0e694437a1f8f8b03c7c3d35e40f127336effe
lymi <269442394@qq.com>
2019-10-02 08:43:28 +0800

    perfect squares

--- dp/279/solution.c
+++ dp/279/solution.c
@@ -3,29 +3,26 @@
 #include <math.h>
 
 /**
- *
  * 1. DP[i] 表示数字i能分解几个数平方和需要的最少数字个数。
- * 2. DP[i] =  min{ DP[j] + DP[i-j] }
+ * 2. DP[i + j*j] =  min{ DP[i+j*j], DP[i] + 1 }
  */
+
+int min(int x, int y) {
+  return x < y ? x : y;
+}
+
 int numSquares(int n) {
   int MAX = n + 1;
-  int DP[n+1], temp;
+  int DP[n+1];
 
-  for (int i = 0; i <= n; i++) {
+  DP[0] = 0;
+  for (int i = 1; i <= n; i++) {
     DP[i] = MAX;
   }
 
-  for (int i = 1; i <= sqrt(n); i++) {
-    DP[i * i] = 1;
-  }
-
-  for (int i = 1; i <= n; i++) {
-    if (DP[i] == 1) continue;
-    for (int j = 1; j <= i / 2; j++) {
-      temp = DP[j] + DP[i-j];
-      if (DP[i] > temp) {
-        DP[i] = temp;
-      }
+  for (int i = 0; i <= n; i++) {
+    for (int j = 1; i + j*j <= n; j++) {
+      DP[i+j*j] = min(DP[i+j*j], DP[i]+1);
     }
   }
 
