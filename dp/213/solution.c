72bbfdd993851a7af7be52046800c2ccbc7447d5
lymi <269442394@qq.com>
2019-10-03 00:22:03 +0800

    house robber

--- dp/213/solution.c
+++ dp/213/solution.c
@@ -13,8 +13,8 @@ int max(int x, int y) {
 }
 
 int compute(int *nums, int first, int last) {
-  int N = last - first + 1;
-  int DP[N];
+  if (first == last) return nums[first];
+  int DP[last+1];
   DP[first] = nums[first];
   DP[first+1] = max(nums[first], nums[first+1]);
   
@@ -34,8 +34,10 @@ int rob(int *nums, int N) {
 int main() {
   int nums1[3] = {2,3,2};
   int nums2[4] = {1,2,3,1};
+  int nums3[2] = {1,3};
   assert(rob(nums1, 3) == 3);
   assert(rob(nums2, 4) == 4);
+  assert(rob(nums3, 2) == 3);
 
   printf("ALL TESTS PASSED!\n");
   return 0;
