935b13a1fda69ab2b626115461f4dab1d43ab73f
lymi <269442394@qq.com>
2019-10-04 01:29:01 +0800

    letter combination of phone numbers

--- search/017/Solution.java
+++ search/017/Solution.java
@@ -4,7 +4,7 @@ class Solution {
   private static final String[] KEYS = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
   private int N;
 
-  public void backtrace(StringBuilder prefix, List<StringBuilder> ret, String digits) {
+  public void backtrace(String prefix, List<String> ret, String digits) {
     int len = prefix.length();
     if (len == N) {
       ret.add(prefix);
@@ -15,20 +15,20 @@ class Solution {
     String str = KEYS[index];
 
     for (char c : str.toCharArray()) {
-      prefix.append(c);
+      prefix += c;
       backtrace(prefix, ret, digits);
-      prefix.deleteCharAt(prefix.length() - 1);
+      prefix.substring(0, len);
     }
   }
 
   public List<String> letterCombinations(String digits) {
-    List<StringBuilder> ret = new ArrayList<>();
+    List<String> ret = new ArrayList<>();
     N = digits.length();
 
     if (digits == null || N == 0) return ret;
 
-    backtrace(new StringBuilder(), ret, digits);
-    return ret;
+    backtrace(new String(), ret, digits);
+    return ret.toString();
   }
 }
 
