2eb52970c0a4f18e36cabdbb591712a97316a770
lymi <269442394@qq.com>
2019-10-03 22:12:17 +0800

    friend circles

--- search/547/Solution.java
+++ search/547/Solution.java
@@ -1,30 +1,26 @@
 class Solution {
-  private int M, N, grid[][];
+  private int N, grid[][], visited[];
 
-  public void dfs(int i, int j) {
-    if (i < 0 || i >= M || j < 0 || j >= N || grid[i][j] == 0) {
-      return;
+  public void dfs(int i) {
+    visited[i] = 1;
+    for (int j = 0; j < N; j++) {
+      if (grid[i][j] == 1 && visited[j] == 0) {
+        dfs(j);
+      }
     }
-    grid[i][j] = 0;
-    dfs(i-1, j);
-    dfs(i+1, j);
-    dfs(i, j-1);
-    dfs(i, j+1);
   }
 
   public int findCircleNum(int[][] data) {
     grid = data;
-    M = grid.length;
-    if (M == 0) return 0;
-    N = grid[0].length;
+    N = grid.length;
+    if (N == 0) return 0;
+    visited = new int[N];
     int cnt = 0;
 
-    for (int i = 0; i < M; i++) {
-      for (int j = 0; j < N; j++) {
-        if (grid[i][j] == 1) {
-          cnt++;
-          dfs(i,j);
-        }
+    for (int i = 0; i < N; i++) {
+      if (visited[i] == 0) {
+        dfs(i);
+        cnt++;
       }
     }
 
@@ -33,10 +29,13 @@ class Solution {
 
   public static void main(String args[]) {
     int grid[][] = {
-     {1,1,0},
-     {1,1,0},
-     {0,0,1}};
-    assert new Solution().findCircleNum(grid) == 2;
+      {1,0,0,1},
+      {0,1,1,0},
+      {0,1,1,1},
+      {1,0,1,1}
+    };
+
+    assert new Solution().findCircleNum(grid) == 1;
     System.out.println("ALL TESTS PASSED!");
   } 
 }
