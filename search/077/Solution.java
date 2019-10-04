c941ef7fc64457222befca4659d24bd07faef10b
lymi <269442394@qq.com>
2019-10-04 20:52:00 +0800

    combinations 2

--- search/077/Solution.java
+++ search/077/Solution.java
@@ -5,19 +5,19 @@ class Solution {
   private boolean[] visited;
   List<List<Integer>> ret = new ArrayList<>();
 
-  public void backtrace(List<Integer> list) {
+  public void backtrace(List<Integer> list, int start) {
     if (list.size() == K) {
       ret.add(new ArrayList<Integer>(list));
     }
 
-    for (int i = 1; i <= N; i++) {
+    for (int i = start; i <= N; i++) {
       if (visited[i]) {
         return;
       }
 
       visited[i] = true;
       list.add(i);
-      backtrace(list);
+      backtrace(list,start+1);
       list.remove(list.size() - 1);
       visited[i] = false;
     }
@@ -28,7 +28,7 @@ class Solution {
     K = k; 
     if (N == 0) return ret;
     visited = new boolean[N+1];
-    backtrace(new ArrayList<Integer>());
+    backtrace(new ArrayList<Integer>(), 1);
     return ret;
   }
 
