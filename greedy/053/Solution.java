3f42b73f1d9738e65ff4abac6ac101825d3acba6
加菲 <jiafei@jiafeideMacBook-Air.local>
2019-10-09 00:39:02 +0800

    max subarray

---
+++ greedy/053/Solution.java
@@ -0,0 +1,21 @@
+/**
+ * 53. Maximum Subarray
+ * Given an integer array nums, find the contiguous subarray
+ * (containing at least one number) which has the largest sum
+ * and return its sum.
+ */
+
+class Solution {
+	public int maxSubArray(int[] nums) {
+		int N = nums.length;
+		int sum, max;
+		sum = max = nums[0];
+
+		for (int i = 1; i < N; i++) {
+			sum = Math.max(sum + nums[i], nums[i]);
+			if (sum > max) max = sum;
+		}
+
+		return max;
+	}
+}
