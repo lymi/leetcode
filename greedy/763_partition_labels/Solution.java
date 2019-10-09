fb9e049076391e8abdc07a922d8d4974b71143b7
lymi <269442394@qq.com>
2019-10-09 15:01:16 +0800

    partition labels

--- greedy/763_partition_labels/Solution.java
+++ greedy/763_partition_labels/Solution.java
@@ -6,4 +6,29 @@
  * representing the size of these parts.
  */
 
+class Solution {
+	public List<Integer> partitionLabels(String S) {
+		int N = S.length(), cnt = 0;
+		List<Integer> ans = new ArrayList<>();
+		int lastPos[] = new int[26]; // 字符c在字符串中最后出现的位置
+		for (int i = 0; i < N; i++) {
+			lastPos[S.charAt(i) - 'a'] = i;
+		}
+
+		int j = lastPos[S.charAt(0) - 'a'];
+		for (int i = 0; i < N; i++) {
+			if (lastPos[S.charAt(i) - 'a'] > j) {
+				j = lastPos[S.charAt(i) - 'a'];
+			}
+
+			cnt++;
+
+			if (i == j) {
+				ans.add(cnt);
+				cnt = 0;
+			}
+		}
+		return ans;
+	}
+}
 
